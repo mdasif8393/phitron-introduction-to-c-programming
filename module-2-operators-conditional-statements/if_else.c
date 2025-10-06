7c012b5adb2173e34932fb0e1d84f29581885da5
mdasif8393 <md.asif.8393@gmail.com>
2025-10-06 17:22:53 +0600

    2-7 If else

---
+++ module-2-operators-conditional-statements/if_else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+
+    int tk = 169;
+
+    if(tk > 100){
+        printf("Burger Khabo");
+    }
+    else{
+        printf("Burger Khabo na");
+    }
+
+    return 0;
+}
