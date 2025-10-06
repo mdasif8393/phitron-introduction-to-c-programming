c2f12d2734943cfcfe5aa0cbe25f645e20e0cc44
mdasif8393 <md.asif.8393@gmail.com>
2025-10-06 11:29:02 +0600

    arithmetic operator

---
+++ module-2-operators-conditional-statements/arithmetic.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int a = 10;
+    int b = 5;
+
+    int sum = a + b;
+    printf("Summation is: %d\n", sum);
+
+    int sub = a - b;
+    printf("Subtraction is: %d\n", sub);
+
+    int mul = a * b;
+    printf("Multiplication is: %d\n", mul);
+
+    int div = a / b;
+    printf("Division is: %d\n", div);
+
+    return 0;
+}
