15e549a6ab7a056c6718ca0f52a048842de609e9
mdasif8393 <md.asif.8393@gmail.com>
2025-10-07 12:51:54 +0600

    solve problem 4

---
+++ module-2.5-practice-day-1/problem-4.c
@@ -0,0 +1,13 @@
+// In this problem you will be given a floating point number x. You have to print x upto 3 decimal points.
+#include<stdio.h>
+
+int main(){
+
+    float num;
+    scanf("%f", &num);
+
+
+    printf("%.3f", num);
+
+    return 0;
+}
