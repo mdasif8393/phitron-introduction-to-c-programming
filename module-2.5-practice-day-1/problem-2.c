e9056bd0a6a71ac51a35cdb85d0c7c937948944a
mdasif8393 <md.asif.8393@gmail.com>
2025-10-07 12:29:41 +0600

    solve problem 2: In this problem you will be given an integer number N. You will have to add 5 with N and print it.

---
+++ module-2.5-practice-day-1/problem-2.c
@@ -0,0 +1,15 @@
+// In this problem you will be given an integer number N. You will have to add 5 with N and print it.
+
+#include<stdio.h>
+
+int main(){
+
+    int num;
+    scanf("%d", &num);
+
+    int add = num + 5;
+
+    printf("%d", add);
+
+    return 0;
+}
