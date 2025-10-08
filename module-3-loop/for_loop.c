bd00016dadee0e63458eed0fa177d067bd7b2a89
mdasif8393 <md.asif.8393@gmail.com>
2025-10-08 17:08:27 +0600

    get sum of a number

--- module-3-loop/for_loop.c
+++ module-3-loop/for_loop.c
@@ -5,6 +5,7 @@ int main(){
 
     for(int i = 1; i <= 10; i++){
         printf("%d I am Sorry\n", i);
+        
     }
     return 0;
 }
---
+++ module-3-loop/sum_from_1_to_n.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+
+    for(int i = 1; i<=n; i++){
+        sum = sum + i;
+        printf("%d\n", sum);
+    }
+
+    printf("%d", sum);
+    
+    return 0;
+}
