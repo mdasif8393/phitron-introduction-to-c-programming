9bb46c4cbc70e544d977076d0545f7fb7a1940a4
mdasif8393 <md.asif.8393@gmail.com>
2025-10-09 13:23:43 +0600

    practice problem 2 solve

--- module-3.5-week-1-practice-day/problem-2.c
+++ module-3.5-week-1-practice-day/problem-2.c
@@ -1,8 +1,15 @@
-
+// Take two integers A and B as input and output their summation.
 
 #include<stdio.h>
 
 int main(){
+
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    int sum = A + B;
+
+    printf("%d", sum);
 
     return 0;
 }
