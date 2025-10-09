a1fac2b64a5a55458854fb3fc945ab627217efd1
mdasif8393 <md.asif.8393@gmail.com>
2025-10-09 13:40:22 +0600

    practice problem 4 solve

--- module-3.5-week-1-practice-day/problem-4.c
+++ module-3.5-week-1-practice-day/problem-4.c
@@ -1,8 +1,16 @@
+// I know and you also know that you love practice day so much. So this task is for you. You will be given a positive integer N, you need to print "I Love Practice" N times.
 
 
 #include<stdio.h>
 
 int main(){
+
+    int N;
+    scanf("%d", &N);
+
+    for(int i =1; i <= N; i++){
+        printf("I Love Practice\n");
+    }
 
     return 0;
 }
