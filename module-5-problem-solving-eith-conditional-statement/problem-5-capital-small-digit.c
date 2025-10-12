d6d03f301ea379bef2ad09ce5a997a96862729c2
mdasif8393 <md.asif.8393@gmail.com>
2025-10-12 12:54:03 +0600

    Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case.

---
+++ module-5-problem-solving-eith-conditional-statement/problem-5-capital-small-digit.c
@@ -0,0 +1,23 @@
+// Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case.
+
+#include<stdio.h>
+
+int main(){
+    char ch;
+    scanf("%c", &ch);
+
+    if(ch >= '0' && ch <= '9'){
+        printf("IS DIGIT");
+    }
+    else{
+        printf("ALPHA\n");
+        if(ch >= 'a' && ch <= 'z'){
+            printf("IS SMALL");
+        }
+        else{
+            printf("IS CAPITAL");
+        }
+    }
+    
+    return 0;
+}
