f79c84db4819732b1142333524ff2e62a5f879fb
mdasif8393 <md.asif.8393@gmail.com>
2025-10-14 14:18:52 +0600

    Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/D

---
+++ module-6.5-practice-day-1/D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,33 @@
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/D
+
+#include<stdio.h>
+
+int main(){
+
+    long long a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+    if(a + b -c == d){
+        printf("YES");
+    }
+    else if(a + b * c == d){
+        printf("YES");
+    }
+    else if(a - b + c == d){
+        printf("YES");
+    }
+    else if(a - b * c == d){
+        printf("YES");
+    }
+    else if(a * b + c == d){
+        printf("YES");
+    }
+    else if(a * b - c == d){
+        printf("YES");
+    }
+    else{
+       printf("NO"); 
+    }
+    
+    return 0;
+}
