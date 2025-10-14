750533a4f2f3d62405f1fc19b6e7300522c5b761
mdasif8393 <md.asif.8393@gmail.com>
2025-10-14 11:36:02 +0600

    solve problem from codeforce link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/G

---
+++ module-6-problem-solving-with-loop/G_Factorial.c
@@ -0,0 +1,29 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/G
+
+#include<stdio.h>
+
+int main(){
+    int x;
+    scanf("%d", &x);
+
+    for(int i =1; i<=x; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        long long fac = 1;
+
+        if(n == 0){
+           fac = 1;
+        }
+        else
+        {
+             for(int i =1; i<=n; i++)
+            {
+                fac = fac * i;
+            } 
+        }
+        printf("%lld\n", fac);
+    }
+    
+    return 0;
+}
