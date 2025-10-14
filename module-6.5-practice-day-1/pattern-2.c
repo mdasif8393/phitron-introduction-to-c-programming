d9a7b4eebeeeeb6626e777135ea67c06a056c774
mdasif8393 <md.asif.8393@gmail.com>
2025-10-14 17:34:38 +0600

    Problem link: https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm86d15s50092r001qeu2gb0t?language=c_103

---
+++ module-6.5-practice-day-1/pattern-2.c
@@ -0,0 +1,18 @@
+// Problem link: https://judge.phitron.io/topics/cm5z7wa3u0007p301xbzdqkuk/cm86d15s50092r001qeu2gb0t?language=c_103
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+
+    for(int i=n; i>=1; i--){
+        for(int j=i ; j>=1; j--){
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
