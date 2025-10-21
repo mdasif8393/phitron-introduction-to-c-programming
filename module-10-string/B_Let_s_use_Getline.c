61cd2d40a4a53e809a7956394004b17e31aadc5b
mdasif8393 <md.asif.8393@gmail.com>
2025-10-21 19:00:50 +0600

    Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/B

---
+++ module-10-string/B_Let_s_use_Getline.c
@@ -0,0 +1,17 @@
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/B
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[1000001];
+    fgets(s, 1000001, stdin);
+
+    for (int i = 0; s[i] != '\\'; i++)
+    {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
