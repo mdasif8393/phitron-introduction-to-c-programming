ec0ae92f7d57aac3f51219b654a493d236f78a3a
mdasif8393 <md.asif.8393@gmail.com>
2025-10-22 12:06:40 +0600

    solve code force problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/G

---
+++ module-10-string/G_Conversion.c
@@ -0,0 +1,28 @@
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/G
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[100001];
+    scanf("%s", &s);
+
+    int s_length = strlen(s);
+
+    for (int i = 0; i < s_length; i++)
+    {
+        if (s[i] == ',')
+        {
+            printf("%c", s[i] - 12);
+        }
+        if (s[i] >= 97 && s[i] <= 122)
+        {
+            printf("%c", s[i] - 32);
+        }
+        if (s[i] >= 65 && s[i] <= 90)
+        {
+            printf("%c", s[i] + 32);
+        }
+    }
+    return 0;
+}
