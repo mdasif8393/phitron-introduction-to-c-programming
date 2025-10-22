3b7889e22106e674e2da686fb5dac5d3ee003a0e
mdasif8393 <md.asif.8393@gmail.com>
2025-10-22 19:37:06 +0600

    copy string

---
+++ module-11-string-operations/string_copy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[100], b[100];
+    scanf("%s %s", &a, &b);
+
+    int length = strlen(b);
+
+    for (int i = 0; i <= length; i++)
+    {
+        a[i] = b[i];
+    }
+
+    printf("%s %s", a, b);
+
+    return 0;
+}
