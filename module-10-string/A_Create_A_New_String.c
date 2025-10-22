0d198be622a0978f68824b0e5156677b4e9a9898
mdasif8393 <md.asif.8393@gmail.com>
2025-10-22 10:56:26 +0600

    solve code force problem: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A

--- module-10-string/A_Create_A_New_String.c
+++ module-10-string/A_Create_A_New_String.c
@@ -1,8 +1,18 @@
-// problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
+// Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/A
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
+    char s1[1001];
+    char s2[1001];
 
+    scanf("%s %s", &s1, &s2);
+
+    int s1_length = strlen(s1);
+    int s2_length = strlen(s2);
+
+    printf("%d %d\n", s1_length, s2_length);
+    printf("%s %s", s1, s2);
     return 0;
 }
