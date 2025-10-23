1b5e76a4341d3acc9edabcb44db5e5eb5cddc5aa
mdasif8393 <md.asif.8393@gmail.com>
2025-10-23 15:35:48 +0600

    implement compare string

---
+++ module-11-string-operations/strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char a[101], b[101];
+    scanf("%s %s", &a, &b);
+
+    int val = (strcmp(a, b));
+
+    if (val < 0)
+    {
+        printf("A is smaller");
+    }
+    else if (val == 0)
+    {
+        printf("Equal");
+    }
+    else if (val > 0)
+    {
+        printf("B is Smaller");
+    }
+
+    return 0;
+}
