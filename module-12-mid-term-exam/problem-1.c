1b66d35059100e6fa7b6170ffe5c52e2ad91a6ae
mdasif8393 <md.asif.8393@gmail.com>
2025-10-25 13:17:27 +0600

    compplete mid term assignment problem number 1. Problem link: https://www.hackerrank.com/contests/mid-term-a-introduction-to-c-programming-a-batch-08/challenges/count-me-2-1

---
+++ module-12-mid-term-exam/problem-1.c
@@ -0,0 +1,39 @@
+// Problem Link: https://www.hackerrank.com/contests/mid-term-a-introduction-to-c-programming-a-batch-08/challenges/count-me-2-1
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int two = 0;
+    int three = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (a[i] % 2 == 0 && a[i] % 3 == 0)
+        {
+            two++;
+        }
+        else if (a[i] % 3 == 0)
+        {
+            three++;
+        }
+        else if (a[i] % 2 == 0)
+        {
+            two++;
+        }
+    }
+
+    printf("%d %d", two, three);
+
+    return 0;
+}
