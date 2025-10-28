acc93533e5f6c0713be5ab188ea6d5747efadaac
mdasif8393 <md.asif.8393@gmail.com>
2025-10-28 12:15:26 +0600

    solve problem 7

---
+++ module-12.5-extra-practice-problems/problem-8.c
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/contests/practice-problem-a-introduction-to-c-programming-a-batch-08/challenges/salami-again
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int max = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > max)
+        {
+            max = a[i];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", max - a[i]);
+    }
+
+    return 0;
+}
