421dd39b9195fadae9e7205e5f5f13a192baf879
mdasif8393 <md.asif.8393@gmail.com>
2025-10-28 10:27:49 +0600

    solve problem 1

---
+++ module-12.5-extra-practice-problems/problem-1.c
@@ -0,0 +1,15 @@
+// Problem Link: https://www.hackerrank.com/contests/practice-problem-a-introduction-to-c-programming-a-batch-08/challenges/salami
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    int result = a + b + c;
+
+    printf("%d", result);
+
+    return 0;
+}
