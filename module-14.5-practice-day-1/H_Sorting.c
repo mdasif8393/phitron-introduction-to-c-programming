1fa1866b75c81417256d1ef5227f03e114e1fc25
mdasif8393 <md.asif.8393@gmail.com>
2025-11-03 21:42:03 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/W

---
+++ module-14.5-practice-day-1/H_Sorting.c
@@ -0,0 +1,37 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/H
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int temp;
+            if (a[j] < a[i])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
