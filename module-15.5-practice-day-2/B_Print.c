e0c44f9c6f2354df636f866762582506b024942d
mdasif8393 <md.asif.8393@gmail.com>
2025-11-06 14:18:26 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/B

--- module-15.5-practice-day-2/B_Print.c
+++ module-15.5-practice-day-2/B_Print.c
@@ -1,1 +1,20 @@
 // https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/B
+#include <stdio.h>
+
+void print(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", i);
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    print(n);
+
+    return 0;
+}
