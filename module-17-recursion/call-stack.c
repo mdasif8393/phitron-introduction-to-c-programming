6eb0c9a22a97dd7a1515f0f5a7d913259b0f3d9b
mdasif8393 <md.asif.8393@gmail.com>
2025-11-08 23:05:46 +0600

    call stack

---
+++ module-17-recursion/call-stack.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void mello()
+{
+    printf("Mello\n");
+}
+
+void gello()
+{
+    printf("Gello\n");
+    mello();
+}
+
+void hello()
+{
+    printf("Hello\n");
+    gello();
+}
+
+int main()
+{
+
+    printf("HI\n");
+    hello();
+
+    return 0;
+}
