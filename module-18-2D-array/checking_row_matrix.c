4c76ec49e2a9c8b801d4688e5c7223bc30a0346a
mdasif8393 <md.asif.8393@gmail.com>
2025-11-11 00:03:39 +0600

    row matrix

---
+++ module-18-2D-array/checking_row_matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    scanf("%d %d", &r, &c);
+
+    int a[r][c];
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if (r == 1)
+    {
+        printf("This is a row Matrix");
+    }
+    else
+    {
+        printf("This is not a row matrix");
+    }
+
+    return 0;
+}
