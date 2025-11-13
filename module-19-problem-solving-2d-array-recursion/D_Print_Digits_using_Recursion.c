7d4d8c1191dc8b84618cbb8a8bd5f93ff1f2104d
mdasif8393 <md.asif.8393@gmail.com>
2025-11-13 18:10:35 +0600

    D. Print Digits using Recursion

---
+++ module-19-problem-solving-2d-array-recursion/D_Print_Digits_using_Recursion.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void recursion(n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    recursion(n / 10);
+    printf("%d ", n % 10);
+}
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+
+        if (n == 0)
+        {
+            printf("0");
+        }
+
+        recursion(n);
+        printf("\n");
+    }
+
+    return 0;
+}
