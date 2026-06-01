85eef0ae112df4eac368d9ff5013a87233730b58
mdasif8393 <md.asif.8393@gmail.com>
2026-06-01 22:20:35 +0600

    reviese module 5

--- module-5-problem-solving-eith-conditional-statement/problem-4-char.c
+++ module-5-problem-solving-eith-conditional-statement/problem-4-char.c
@@ -1,18 +1,21 @@
 //* Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    
+int main()
+{
+
     char ch;
     scanf("%c", &ch);
 
     char result;
 
-    if(ch >= 'a' && ch <= 'z'){
-        printf("%c", ch -32);
+    if (ch >= 'a' && ch <= 'z')
+    {
+        printf("%c", ch - 32);
     }
-    else{
+    else
+    {
         printf("%c", ch + 32);
     }
 
