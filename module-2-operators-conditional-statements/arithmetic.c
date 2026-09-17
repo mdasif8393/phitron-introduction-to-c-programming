fffd59e7e39cd8e375b25a8939facbde4e6aa25e
mdasif8393 <md.asif.8393@gmail.com>
2026-09-17 13:22:22 +0600

    practice 1

--- module-2-operators-conditional-statements/arithmetic.c
+++ module-2-operators-conditional-statements/arithmetic.c
@@ -1,22 +1,34 @@
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int a = 10;
+//     int b = 5;
+
+//     int sum = a + b;
+//     printf("Summation is: %d\n", sum);
+
+//     int sub = a - b;
+//     printf("Subtraction is: %d\n", sub);
+
+//     int mul = a * b;
+//     printf("Multiplication is: %d\n", mul);
+
+//     int div = a / b;
+//     printf("Division is: %d\n", div);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
 
-    int a = 10;
-    int b = 5;
+    int a = 10, b = 5;
 
-    int sum = a + b;
-    printf("Summation is: %d\n", sum);
-
-    int sub = a - b;
-    printf("Subtraction is: %d\n", sub);
-
-    int mul = a * b;
-    printf("Multiplication is: %d\n", mul);
-
-    int div = a / b;
-    printf("Division is: %d\n", div);
+    printf("%d", a % b);
 
     return 0;
 }
