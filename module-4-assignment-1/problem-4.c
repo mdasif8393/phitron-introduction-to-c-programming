f45d884e82fe2a54d8dea9a1ef8f7bd1458a985b
mdasif8393 <md.asif.8393@gmail.com>
2026-09-18 18:09:49 +0600

    solve assignment 1 problem 4

--- module-4-assignment-1/problem-4.c
+++ module-4-assignment-1/problem-4.c
@@ -1,18 +1,34 @@
 // You will be given a non-negative integer N, you need to print all numbers from 1 to N that are divisible by both 3 and 7.
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
 
-    int N;
-    scanf("%d", &N);
+//     int N;
+//     scanf("%d", &N);
 
-    for(int i =1; i <= N; i++){
-        if( (i % 3 == 0) && (i % 7 == 0)){
+//     for(int i =1; i <= N; i++){
+//         if( (i % 3 == 0) && (i % 7 == 0)){
+//             printf("%d\n", i);
+//         }
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 && i % 7 == 0)
+        {
             printf("%d\n", i);
-        }  
+        }
     }
-
-
     return 0;
 }
