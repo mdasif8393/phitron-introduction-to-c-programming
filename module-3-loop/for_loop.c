5418b9450d00e5224c327724f843c1aad607fdbd
mdasif8393 <md.asif.8393@gmail.com>
2026-09-18 10:15:58 +0600

    for loop

--- module-3-loop/for_loop.c
+++ module-3-loop/for_loop.c
@@ -1,14 +1,24 @@
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
 
+//      int i=0,j=0;
+//     for(j=1;j<=5;j++)
+//     {
+//         i++;
+//     }
+//     printf("%d",j);
 
-     int i=0,j=0;
-    for(j=1;j<=5;j++)
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
     {
-        i++;
+        printf("%d\n", i);
     }
-    printf("%d",j);
-
     return 0;
 }
--- module-3-loop/more_for_loop.c
+++ module-3-loop/more_for_loop.c
@@ -1,9 +1,20 @@
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     for (int i = 2; i <= 100; i = i * 2)
+//     {
+//         printf("%d\n", i);
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-
-    for (int i = 2; i <= 100; i = i * 2)
+    for (int i = 100; i >= 1; i--)
     {
         printf("%d\n", i);
     }
