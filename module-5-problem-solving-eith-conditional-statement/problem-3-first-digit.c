278d714bf636e0a76aedcfdc4eca7851742a6c80
mdasif8393 <md.asif.8393@gmail.com>
2026-09-18 18:31:19 +0600

    solve problem 3

--- module-5-problem-solving-eith-conditional-statement/problem-3-first-digit.c
+++ module-5-problem-solving-eith-conditional-statement/problem-3-first-digit.c
@@ -1,21 +1,41 @@
 //* Given a number X. Print "EVEN" if the first digit of X is even number. Otherwise print "ODD".
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
 
-    int x;
+//     int x;
 
-    scanf("%d", &x);
+//     scanf("%d", &x);
 
-    int firstDigit = x / 1000;
+//     int firstDigit = x / 1000;
 
-    if(firstDigit % 2 == 0){
+//     if(firstDigit % 2 == 0){
+//         printf("EVEN");
+//     }
+//     else{
+//         printf("ODD");
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int firstDigit = n / 1000;
+
+    if (firstDigit % 2 == 0)
+    {
         printf("EVEN");
     }
-    else{
+    else
+    {
         printf("ODD");
     }
-    
     return 0;
 }
