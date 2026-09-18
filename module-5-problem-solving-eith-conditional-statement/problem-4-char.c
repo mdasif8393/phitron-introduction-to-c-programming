ddb85078c16702efd56b7927c9a039e20533d0b0
mdasif8393 <md.asif.8393@gmail.com>
2026-09-18 18:47:52 +0600

    solve problem 4

--- module-5-problem-solving-eith-conditional-statement/problem-4-char.c
+++ module-5-problem-solving-eith-conditional-statement/problem-4-char.c
@@ -1,23 +1,34 @@
 //* Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
 
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     char ch;
+//     scanf("%c", &ch);
+
+//     char result;
+
+//     if (ch >= 'a' && ch <= 'z')
+//     {
+//         printf("%c", ch - 32);
+//     }
+//     else
+//     {
+//         printf("%c", ch + 32);
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
+    char c;
+    scanf("%c", &c);
 
-    char ch;
-    scanf("%c", &ch);
-
-    char result;
-
-    if (ch >= 'a' && ch <= 'z')
-    {
-        printf("%c", ch - 32);
-    }
-    else
-    {
-        printf("%c", ch + 32);
-    }
-
+    printf("%c", c - 32);
     return 0;
 }
--- module-5-problem-solving-eith-conditional-statement/problem-5-capital-small-digit.c
+++ module-5-problem-solving-eith-conditional-statement/problem-5-capital-small-digit.c
@@ -1,23 +1,49 @@
 // Given a letter X. Determine whether X is Digit or Alphabet and if it is Alphabet determine if it is Capital Case or Small Case.
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
-    char ch;
-    scanf("%c", &ch);
+// int main(){
+//     char ch;
+//     scanf("%c", &ch);
 
-    if(ch >= '0' && ch <= '9'){
-        printf("IS DIGIT");
-    }
-    else{
+//     if(ch >= '0' && ch <= '9'){
+//         printf("IS DIGIT");
+//     }
+//     else{
+//         printf("ALPHA\n");
+//         if(ch >= 'a' && ch <= 'z'){
+//             printf("IS SMALL");
+//         }
+//         else{
+//             printf("IS CAPITAL");
+//         }
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    scanf("%c", &c);
+
+    if (c >= 65 && c <= 122)
+    {
         printf("ALPHA\n");
-        if(ch >= 'a' && ch <= 'z'){
-            printf("IS SMALL");
-        }
-        else{
+        if (c >= 65 && c <= 90)
+        {
             printf("IS CAPITAL");
         }
+        else
+        {
+            printf("IS SMALL");
+        }
     }
-    
+    else
+    {
+        printf("IS DIGIT");
+    }
     return 0;
 }
