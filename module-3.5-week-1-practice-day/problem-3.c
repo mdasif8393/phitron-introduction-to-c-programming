66b578dc44d82d6546ca110c3f4ef4f459ac3bfc
mdasif8393 <md.asif.8393@gmail.com>
2026-09-18 17:26:06 +0600

    solve problem 5

--- module-3.5-week-1-practice-day/problem-3.c
+++ module-3.5-week-1-practice-day/problem-3.c
@@ -1,20 +1,35 @@
 // You've learned about variables, right? Now its time to practice them. You need to take an integer A, a very big integer B, a floating value  C and a character D as input and output them serially.
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
 
-    int A;
-    long long int B;
-    double C;
-    char D;
-    
-    scanf("%d %ld %lf %c", &A, &B, &C, &D);
+//     int A;
+//     long long int B;
+//     double C;
+//     char D;
 
-    printf("%d\n", A);
-    printf("%lld\n", B);
-    printf("%.2lf\n", C);
-    printf("%c\n", D);
+//     scanf("%d %ld %lf %c", &A, &B, &C, &D);
 
+//     printf("%d\n", A);
+//     printf("%lld\n", B);
+//     printf("%.2lf\n", C);
+//     printf("%c\n", D);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    long long int b;
+    double c;
+    char d;
+
+    scanf("%d %ld %lf %c", &a, &b, &c, &d);
+
+    printf("%d\n %ld\n %.2lf\n %c\n", a, b, c, d);
     return 0;
 }
--- module-3.5-week-1-practice-day/problem-4.c
+++ module-3.5-week-1-practice-day/problem-4.c
@@ -1,16 +1,29 @@
 // I know and you also know that you love practice day so much. So this task is for you. You will be given a positive integer N, you need to print "I Love Practice" N times.
 
+// #include<stdio.h>
 
-#include<stdio.h>
+// int main(){
 
-int main(){
+//     int N;
+//     scanf("%d", &N);
 
-    int N;
-    scanf("%d", &N);
+//     for(int i =1; i <= N; i++){
+//         printf("I Love Practice\n");
+//     }
 
-    for(int i =1; i <= N; i++){
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
         printf("I Love Practice\n");
     }
-
     return 0;
 }
