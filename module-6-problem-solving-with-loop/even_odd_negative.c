091b344b37f3a9502fe692afccc5249b35910a3b
mdasif8393 <md.asif.8393@gmail.com>
2026-09-19 13:58:56 +0600

    solve: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/C

--- module-6-problem-solving-with-loop/even_odd_negative.c
+++ module-6-problem-solving-with-loop/even_odd_negative.c
@@ -1,41 +1,81 @@
 // Given N numbers. Count how many of these values are even, odd, positive and negative.
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
-    
+// int main(){
+
+//     int n;
+//      int x;
+//      int even = 0;
+//      int odd = 0;
+//      int positive = 0;
+//      int negative = 0;
+
+//     scanf("%d", &n);
+
+//     for(int i=1; i<=n; i++){
+//         scanf("%d", &x);
+
+//         if(x % 2 == 0){
+//             even ++;
+//         }
+//         if(x % 2 != 0){
+//             odd ++;
+//         }
+//         if( x > 0 ){
+//             positive ++;
+//         }
+//         if( x < 0 ){
+//             negative ++;
+//         }
+
+//     }
+//      printf("Even: %d\n", even);
+//      printf("Odd: %d\n", odd);
+//      printf("Positive: %d\n", positive);
+//      printf("Negative: %d\n", negative);
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
     int n;
-     int x;
-     int even = 0;
-     int odd = 0;
-     int positive = 0;
-     int negative = 0;
-    
     scanf("%d", &n);
 
-    for(int i=1; i<=n; i++){
+    int even = 0;
+    int odd = 0;
+    int positive = 0;
+    int negative = 0;
+
+    while (n--)
+    {
+        int x;
         scanf("%d", &x);
 
-        if(x % 2 == 0){
-            even ++;
+        if (x % 2 == 0)
+        {
+            even++;
         }
-        if(x % 2 != 0){
-            odd ++;
+        else if (x % 2 != 0)
+        {
+            odd++;
         }
-        if( x > 0 ){
-            positive ++;
+        else if (x > 0)
+        {
+            positive++;
         }
-        if( x < 0 ){
-            negative ++;
+        else if (x < 0)
+        {
+            negative++;
         }
-       
     }
-     printf("Even: %d\n", even);
-     printf("Odd: %d\n", odd);
-     printf("Positive: %d\n", positive);
-     printf("Negative: %d\n", negative);
 
-    
-
+    printf("Even: %d\n", even);
+    printf("Odd: %d\n", odd);
+    printf("Negative: %d\n", negative);
+    printf("Positive: %d\n", positive);
     return 0;
 }
