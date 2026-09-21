42e42925cf69d80d5e58ea59052f22e8404d837c
mdasif8393 <md.asif.8393@gmail.com>
2026-09-21 11:46:38 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/S

--- module-6-problem-solving-with-loop/S_Sum_of_Consecutive_Odd_Numbers.c
+++ module-6-problem-solving-with-loop/S_Sum_of_Consecutive_Odd_Numbers.c
@@ -1,32 +1,71 @@
 // Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/S
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
+//     int n;
+//     scanf("%d", &n);
+
+//     for(int i=1; i<=n; i++){
+//         int x,y;
+//         scanf("%d %d", &x, &y);
+
+//         int sum = 0;
+//         if(x<y){
+//             for(int i=x+1; i<y; i++){
+//                 if(i%2 != 0){
+//                     sum = sum + i;
+//                 }
+//             }
+
+//         }
+//         else{
+//             for(int i=y+1; i<x; i++){
+//             if(i%2 != 0){
+//                 sum = sum + i;
+//             }
+//             }
+//             }
+//         printf("%d\n", sum);
+//         }
+
+//     return 0;
+// }
+#include <stdio.h>
+
+int main()
+{
     int n;
     scanf("%d", &n);
 
-    for(int i=1; i<=n; i++){
-        int x,y;
+    while (n--)
+    {
+        int x, y;
         scanf("%d %d", &x, &y);
 
+        int max, min;
+
+        if (x > y)
+        {
+            max = x;
+            min = y;
+        }
+        else
+        {
+            max = y;
+            min = x;
+        }
+
         int sum = 0;
-        if(x<y){
-            for(int i=x+1; i<y; i++){
-                if(i%2 != 0){
-                    sum = sum + i;
-                }
+        for (int i = min + 1; i < max; i++)
+        {
+            if (i % 2 != 0)
+            {
+                sum += i;
             }
-            
         }
-        else{
-            for(int i=y+1; i<x; i++){
-            if(i%2 != 0){
-                sum = sum + i;
-            }
-            }
-            }
+
         printf("%d\n", sum);
-        }
-    
+    }
+
     return 0;
 }
