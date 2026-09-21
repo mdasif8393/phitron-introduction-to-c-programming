8ed9e68f5cd807f6f87406006385fbd83a92e8aa
mdasif8393 <md.asif.8393@gmail.com>
2026-09-21 11:08:22 +0600

    solve problem: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/K

--- module-6-problem-solving-with-loop/K_Divisors.c
+++ module-6-problem-solving-with-loop/K_Divisors.c
@@ -1,13 +1,30 @@
 // problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/K
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
+//     int n;
+//     scanf("%d", &n);
+
+//     for(int i=1; i<=n; i++){
+//         if( n%i == 0 ){
+//             printf("%d\n", i);
+//         }
+//     }
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
     int n;
     scanf("%d", &n);
 
-    for(int i=1; i<=n; i++){
-        if( n%i == 0 ){
+    for (int i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+        {
             printf("%d\n", i);
         }
     }
