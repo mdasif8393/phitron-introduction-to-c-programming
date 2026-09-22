b8b8c88c3edd3ce4f1281b553c65257ae276685e
mdasif8393 <md.asif.8393@gmail.com>
2026-09-22 11:15:46 +0600

    solve b

--- module-7.5-practice-day-2/B_Searching.c
+++ module-7.5-practice-day-2/B_Searching.c
@@ -1,10 +1,42 @@
 // Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/B
 
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     int compare;
+//     scanf("%d", &compare);
+
+//     int pos = -1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (compare == a[i])
+//         {
+//             pos = i;
+//             break;
+//         }
+//     }
+
+//     printf("%d", pos);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-
     int n;
     scanf("%d", &n);
 
@@ -14,14 +46,14 @@ int main()
         scanf("%d", &a[i]);
     }
 
-    int compare;
-    scanf("%d", &compare);
+    int x;
+    scanf("%d", &x);
 
     int pos = -1;
 
     for (int i = 0; i < n; i++)
     {
-        if (compare == a[i])
+        if (a[i] == x)
         {
             pos = i;
             break;
