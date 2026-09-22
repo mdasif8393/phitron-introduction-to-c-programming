91dc21e337da8158319b1a18daf61398a2f2660c
mdasif8393 <md.asif.8393@gmail.com>
2026-09-22 09:48:09 +0600

    solve practice 1

--- module-7-array/practice-1.c
+++ module-7-array/practice-1.c
@@ -1,23 +1,47 @@
 // Take an array from input and print all the even numbers of that array.
 
-#include<stdio.h>
+// #include<stdio.h>
 
-int main(){
+// int main(){
 
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for(int i=0; i<n; i++){
+//         scanf("%d", &a[i]);
+//     }
+
+//     for(int i=0; i<n; i++){
+//         if(a[i] % 2 == 0){
+//             printf("%d ", a[i]);
+//         }
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main()
+{
     int n;
     scanf("%d", &n);
 
     int a[n];
-
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         scanf("%d", &a[i]);
     }
 
-    for(int i=0; i<n; i++){
-        if(a[i] % 2 == 0){
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] % 2 == 0)
+        {
             printf("%d ", a[i]);
         }
     }
-    
+
     return 0;
 }
