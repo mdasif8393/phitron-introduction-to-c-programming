4742c0da6a34b23087836fb2dcbbbe9a4811f3c6
mdasif8393 <md.asif.8393@gmail.com>
2026-09-23 10:06:20 +0600

    awap array variables

--- module-9-array-operations/insert_value.c
+++ module-9-array-operations/insert_value.c
@@ -1,9 +1,42 @@
 // insert a element in any position of an array
 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n + 1];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     int idx, val;
+//     scanf("%d %d", &idx, &val);
+
+//     for (int i = n; i >= idx + 1; i--)
+//     {
+//         a[i] = a[i - 1];
+//     }
+
+//     a[idx] = val;
+
+//     for (int i = 0; i < n + 1; i++)
+//     {
+//         printf("%d ", a[i]);
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
+
     int n;
     scanf("%d", &n);
 
@@ -24,7 +57,7 @@ int main()
 
     a[idx] = val;
 
-    for (int i = 0; i < n + 1; i++)
+    for (int i = 0; i <= n; i++)
     {
         printf("%d ", a[i]);
     }
--- module-9-array-operations/remove_value.c
+++ module-9-array-operations/remove_value.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
 
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+//     int idx;
+//     scanf("%d ", &idx);
+//     for (int i = idx; i < n - 1; i++)
+//     {
+//         a[i] = a[i + 1];
+//     }
+//     n--;
+//     for (int i = 0; i < n; i++)
+//     {
+//         printf("%d ", a[i]);
+//     }
+
+//     return 0;
+// }
+
+#include <stdio.h>
+
 int main()
 {
     int n;
@@ -10,16 +37,12 @@ int main()
     {
         scanf("%d", &a[i]);
     }
+
     int idx;
-    scanf("%d ", &idx);
+    scanf("%d", &idx);
+
     for (int i = idx; i < n - 1; i++)
     {
-        a[i] = a[i + 1];
-    }
-    n--;
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", a[i]);
     }
 
     return 0;
