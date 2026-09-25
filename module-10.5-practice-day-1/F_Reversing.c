63b4b9b25c418be964f6c909fdebda137befbb23
mdasif8393 <md.asif.8393@gmail.com>
2026-09-25 12:13:31 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/F

--- module-10.5-practice-day-1/F_Reversing.c
+++ module-10.5-practice-day-1/F_Reversing.c
@@ -1,8 +1,28 @@
 // Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/F
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+//     for (int i = n - 1; i >= 0; i--)
+//     {
+//         printf("%d ", a[i]);
+//     }
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
+
     int n;
     scanf("%d", &n);
 
@@ -11,9 +31,11 @@ int main()
     {
         scanf("%d", &a[i]);
     }
+
     for (int i = n - 1; i >= 0; i--)
     {
         printf("%d ", a[i]);
     }
+
     return 0;
 }
