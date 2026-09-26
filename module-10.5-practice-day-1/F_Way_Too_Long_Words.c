053f519131c7d7aa030f1f578e31d0daaaa4b06e
mdasif8393 <md.asif.8393@gmail.com>
2026-09-26 10:37:53 +0600

    a

--- module-10.5-practice-day-1/F_Way_Too_Long_Words.c
+++ module-10.5-practice-day-1/F_Way_Too_Long_Words.c
@@ -1,37 +1,65 @@
 // Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/F
 
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         char s[101];
+
+//         scanf("%s", &s);
+
+//         int count = 0;
+
+//         int s_length = strlen(s);
+//         if (s_length <= 10)
+//         {
+//             printf("%s\n", s);
+//         }
+//         else
+//         {
+//             printf("%c", s[0]);
+//             for (int i = 1; i < s_length - 1; i++)
+//             {
+//                 count++;
+//             }
+//             printf("%d", count);
+//             printf("%c\n", s[s_length - 1]);
+//         }
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
+
     int n;
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++)
     {
         char s[101];
+        gets(s);
 
-        scanf("%s", &s);
+        int len = strlen(s);
 
-        int count = 0;
-
-        int s_length = strlen(s);
-        if (s_length <= 10)
+        if (len <= 10)
         {
             printf("%s\n", s);
         }
         else
         {
-            printf("%c", s[0]);
-            for (int i = 1; i < s_length - 1; i++)
-            {
-                count++;
-            }
-            printf("%d", count);
-            printf("%c\n", s[s_length - 1]);
+            printf("%c%d%c\n", s[0], len - 2, s[len - 1]);
         }
     }
-
     return 0;
 }
--- module-10.5-practice-day-1/G_Palindrome_Array.c
+++ module-10.5-practice-day-1/G_Palindrome_Array.c
@@ -1,10 +1,53 @@
 // Problem link: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/G
 
+// #include <stdio.h>
+
+// int main()
+// {
+
+//     int n;
+//     scanf("%d", &n);
+
+//     int a[n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         scanf("%d", &a[i]);
+//     }
+
+//     int b[n];
+
+//     for (int i = 0, j = n - 1; i < n; i++, j--)
+//     {
+//         b[j] = a[i];
+//     }
+
+//     int palindrome = 1;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (a[i] != b[i])
+//         {
+//             palindrome = 0;
+//         }
+//     }
+
+//     if (palindrome)
+//     {
+//         printf("YES");
+//     }
+//     else
+//     {
+//         printf("NO");
+//     }
+
+//     return 0;
+// }
+
 #include <stdio.h>
 
 int main()
 {
-
     int n;
     scanf("%d", &n);
 
@@ -15,24 +58,21 @@ int main()
         scanf("%d", &a[i]);
     }
 
-    int b[n];
+    int i = 0;
+    int j = n - 1;
 
-    for (int i = 0, j = n - 1; i < n; i++, j--)
+    int pal = 1;
+    while (i < j)
     {
-        b[j] = a[i];
-    }
-
-    int palindrome = 1;
-
-    for (int i = 0; i < n; i++)
-    {
-        if (a[i] != b[i])
+        if (a[i] != a[j])
         {
-            palindrome = 0;
+            pal = 0;
         }
+        i++;
+        j--;
     }
 
-    if (palindrome)
+    if (pal == 1)
     {
         printf("YES");
     }
