fe687a53dc6cc8b6786bf4b8423e018567910bfc
mdasif8393 <md.asif.8393@gmail.com>
2026-09-26 11:32:17 +0600

    https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/D

--- module-10.5-practice-day-1/D_Strings.c
+++ module-10.5-practice-day-1/D_Strings.c
@@ -1,25 +1,48 @@
 // Problem Link: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/D
+// #include <stdio.h>
+// #include <string.h>
+
+// int main()
+// {
+
+//     char a[11];
+//     char b[11];
+
+//     scanf("%s", &a);
+//     scanf("%s", &b);
+
+//     int a_len = strlen(a);
+//     int b_len = strlen(b);
+
+//     printf("%d %d\n", a_len, b_len);
+//     printf("%s%s\n", a, b);
+
+//     char temp;
+
+//     temp = a[0];
+//     a[0] = b[0];
+//     b[0] = temp;
+//     printf("%s %s", a, b);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <string.h>
 
 int main()
 {
 
-    char a[11];
-    char b[11];
+    char a[11], b[11];
+    scanf("%s %s", &a, &b);
 
-    scanf("%s", &a);
-    scanf("%s", &b);
+    int len1 = strlen(a);
+    int len2 = strlen(b);
 
-    int a_len = strlen(a);
-    int b_len = strlen(b);
-
-    printf("%d %d\n", a_len, b_len);
+    printf("%d %d\n", len1, len2);
     printf("%s%s\n", a, b);
 
-    char temp;
-
-    temp = a[0];
+    char temp = a[0];
     a[0] = b[0];
     b[0] = temp;
     printf("%s %s", a, b);
